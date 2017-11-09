7f5dc17104725e7815a2b3ef60f864ac7a1ca959
Site Wang <sitew@clemson.edu>
2017-11-09 09:47:14 -0700

    not including no-longer-exist-files

--- src/MINLPLib_jump.jl
+++ src/MINLPLib_jump.jl
@@ -2,11 +2,7 @@ module MINLPLib_jump
 
 using JuMP
 
-include("consts.jl")
 include("features.jl")
-include("types.jl")
-include("utility.jl")
-include("convert.jl")
 
 # Compile all problem instances
 special_instances = ["multi3N", "multi4N", "multiKND", "eniplac", "simpleN", "circleN"]
