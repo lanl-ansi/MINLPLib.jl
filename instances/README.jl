1d78ab5f42471742e7e6d794b26bb461e0ccd027
jac0320 <sitew@g.clemson.edu>
2017-11-23 21:46:18 -0700

    Intermedia commit to checkout the looks

---
+++ instances/README.jl
@@ -0,0 +1,23 @@
+# Instances
+
+Sor far, MINLPLibJuMP includes `16` public libraries in `.jl` format converted from `.gms`.
+Here is a simple chart showing the status of each instance libraries.
+
+| Lib Name | Converted  | Unconverted | % |
+|----------|------------|-------------|---|
+| [PODLib](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/PODLib)     | 82     | -    | - |
+| [bcp](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/bcp)           | 143    | 327  | 43.7% |
+| [global](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/global)     | 356    | 369  | 96.5% |
+| [ibm](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/ibm)           | 142    | 142  | 100% |
+| [inf](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/inf)           | 608    | 608  | 100% |
+| [minlp](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/minlp)       | 110    | 250  | 44.0% |
+| [minlp2](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/minlp2)     | 1513   | 1527 | 99.1% |
+| [morg](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/morg)         | 66     | 67   | 98.5% |
+| [mpec](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/mpec)         | 92     | 92   | 100% |
+| [mult3](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/mult3)       | 120    | 120  | 100% |
+| [mult4](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/mult4)       | 100    | 100  | 100% |
+| [poly](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/poly)         | 98     | 98   | 100% |
+| [prince](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/prince)     | 893    | 980  | 91.1% |
+| [qcqp](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/qcqp)         | 135    | 135  | 100% |
+| [qcqp2](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/qcqp2)       | 700    | 700  | 100% |
+| [qcqp3](https://github.com/lanl-ansi/MINLPLibJuMP.jl/tree/master/instances/qcqp3)       | 320    | 320  | 100% |
---
+++ instances/bcp/README.md
@@ -0,0 +1,12 @@
+# MINLP Instance Library `bcp`
+
+## Description
+Bound-constrained Programs
+
+## Source
+
+
+## Instance Stats
+
+| Name  | `LT`  | <!---SENSE | VARS | BINVARS | INTVARS | CONS | LINCONS | NLCONS |--->
+|-------|-------|
