5141182f232e9edcdfe232c09c10743740b21918
sitewang <sitew@g.clemson.edu>
2018-03-07 01:21:05 -0500

    Replacing main PODLib with reformulated problems

--- instances/PODLib/ex8_1_3.jl
+++ instances/PODLib/ex8_1_3.jl
@@ -1,1 +1,18 @@
-include(joinpath(Pkg.dir("MINLPLibJuMP"),"instances","bcp", "ex8_1_3.jl"))
+using JuMP
+
+m = Model()
+
+# ----- Variables ----- #
+@variable(m, objvar)
+x_Idx = Any[1, 2]
+@variable(m, x[x_Idx])
+
+
+# ----- Constraints ----- #
+@NLconstraint(m, e1, -(1+ (1+x[1]+x[2])^2*(19+3* (x[1])^2-14*x[1]+6*x[1]*x[2]-14*x[2]+3* (x[2])^2))*(30+ (2*x[1]-3*x[2])^2*(18+12* (x[1])^2-32*x[1]-36*x[1]*x[2]+48*x[2]+27* (x[2])^2))+objvar == 0.0)
+
+
+# ----- Objective ----- #
+@objective(m, Min, objvar)
+
+m = m 		 # model get returned when including this script. 
--- instances/PODLib/ex8_1_4.jl
+++ instances/PODLib/ex8_1_4.jl
@@ -1,1 +1,18 @@
-include(joinpath(Pkg.dir("MINLPLibJuMP"),"instances","bcp", "ex8_1_4.jl"))
+using JuMP
+
+m = Model()
+
+# ----- Variables ----- #
+@variable(m, objvar)
+x_Idx = Any[1, 2]
+@variable(m, x[x_Idx])
+
+
+# ----- Constraints ----- #
+@NLconstraint(m, e1, -(12* (x[1])^2-6.3* (x[1])^4+ (x[1])^6-6*x[1]*x[2]+6* (x[2])^2)+objvar == 0.0)
+
+
+# ----- Objective ----- #
+@objective(m, Min, objvar)
+
+m = m 		 # model get returned when including this script. 
