558299170f10f7573571914a5134a9ec77fc39e2
Harsha Nagarajan <harsha.ngrjn@gmail.com>
2019-03-21 22:37:32 -0600

    Update ex1222.jl
    Cleaner formats

--- instances/minlp/ex1222.jl
+++ instances/minlp/ex1222.jl
@@ -5,21 +5,17 @@ m = Model()
 # ----- Variables ----- #
 @variable(m, objvar)
 x_Idx = Any[1, 2]
-@variable(m, x[x_Idx])
+LB = [0.2, -2.22554]
+UB = [1, -1]
+@variable(m, LB[i] <= x[i in x_Idx] <= UB[i])
 b_Idx = Any[3]
-@variable(m, b[b_Idx])
-setcategory(b[3], :Bin)
-setlowerbound(x[1], 0.2)
-setupperbound(x[1], 1.0)
-setlowerbound(x[2], -2.22554)
-setupperbound(x[2], -1.0)
-
+@variable(m, b[b_Idx], Bin)
 
 # ----- Constraints ----- #
 @NLconstraint(m, e1, -exp(x[1]-0.2)-x[2] <= 0.0)
-@constraint(m, e2, x[2]+1.1*b[3] <= -1.0)
-@constraint(m, e3, x[1]-1.2*b[3] <= 0.0)
-@NLconstraint(m, e4, -5* (x[1]-0.5)^2+0.7*b[3]+objvar == 0.8)
+@constraint(m, e2, x[2] + 1.1*b[3] <= -1.0)
+@constraint(m, e3, x[1] - 1.2*b[3] <= 0.0) 
+@NLconstraint(m, e4, -5*(x[1]-0.5)^2 + 0.7*b[3] + objvar == 0.8)
 
 
 # ----- Objective ----- #
