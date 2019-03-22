a59274ce3f60e8635d01af6b7191e0074089ffbe
Harsha Nagarajan <harsha.ngrjn@gmail.com>
2019-03-21 22:40:49 -0600

    Update ex1223.jl
    Cleaner formats

--- instances/minlp/ex1223.jl
+++ instances/minlp/ex1223.jl
@@ -5,28 +5,10 @@ m = Model()
 # ----- Variables ----- #
 @variable(m, objvar)
 x_Idx = Any[1, 2, 3, 4, 5, 6, 7]
-@variable(m, x[x_Idx])
+UB = [10, 10, 10, 1, 1, 1, 1]
+@variable(m, 0 <= x[i in x_Idx] <= UB[i])
 b_Idx = Any[8, 9, 10, 11]
-@variable(m, b[b_Idx])
-setlowerbound(x[4], 0.0)
-setlowerbound(x[6], 0.0)
-setlowerbound(x[3], 0.0)
-setcategory(b[8], :Bin)
-setcategory(b[11], :Bin)
-setcategory(b[9], :Bin)
-setlowerbound(x[5], 0.0)
-setlowerbound(x[2], 0.0)
-setcategory(b[10], :Bin)
-setlowerbound(x[1], 0.0)
-setlowerbound(x[7], 0.0)
-setupperbound(x[1], 10.0)
-setupperbound(x[2], 10.0)
-setupperbound(x[3], 10.0)
-setupperbound(x[4], 1.0)
-setupperbound(x[5], 1.0)
-setupperbound(x[6], 1.0)
-setupperbound(x[7], 1.0)
-
+@variable(m, b[b_Idx], Bin)
 
 # ----- Constraints ----- #
 @constraint(m, e1, x[1]+x[2]+x[3]+b[8]+b[9]+b[10] <= 5.0)
