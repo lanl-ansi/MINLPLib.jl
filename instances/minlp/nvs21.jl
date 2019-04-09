fca495a1a204c11e5ac529470fb2ddbd6c7994b3
Harsha Nagarajan <harsha.ngrjn@gmail.com>
2019-04-09 17:27:55 -0600

    Indices fixed!

--- instances/minlp/nvs21.jl
+++ instances/minlp/nvs21.jl
@@ -4,15 +4,15 @@ m = Model()
 
 # ----- Variables ----- #
 @variable(m, objvar)
-x_Idx = Any[3]
+x_Idx = Any[1]
 @variable(m, 0 <= x[x_Idx] <= 0.2)
 i_Idx = Any[1, 2]
 @variable(m, 0 <= i[i_Idx] <= 200, Int)
 
 # ----- Constraints ----- #
 @NLconstraint(m, e1, - (i[1])^2*i[2] >= -675.0)
-@NLconstraint(m, e2, -0.1* (i[1])^2* (x[3])^2 >= -0.419)
-@NLconstraint(m, e3, 0.00201* (i[1])^4*i[2]* (x[3])^2+objvar == 0.0)
+@NLconstraint(m, e2, -0.1* (i[1])^2* (x[1])^2 >= -0.419)
+@NLconstraint(m, e3, 0.00201* (i[1])^4*i[2]* (x[1])^2+objvar == 0.0)
 
 
 # ----- Objective ----- #
