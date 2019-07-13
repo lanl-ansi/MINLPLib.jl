af9d60c65a89beb26ae10dd434e292565f3cb9de
Harsha Nagarajan <harsha.ngrjn@gmail.com>
2019-07-13 00:26:20 -0400

    [ci skip] clean_up

--- instances/minlp2/nvs08.jl
+++ instances/minlp2/nvs08.jl
@@ -7,15 +7,7 @@ m = Model()
 x_Idx = Any[3]
 @variable(m, x[x_Idx])
 i_Idx = Any[1, 2]
-@variable(m, i[i_Idx])
-setcategory(i[1], :Int)
-setlowerbound(i[1], 0.0)
-setupperbound(i[1], 100.0)
-setcategory(i[2], :Int)
-setlowerbound(i[2], 0.0)
-setupperbound(i[2], 100.0)
-setupperbound(i[1], 200.0)
-setupperbound(i[2], 200.0)
+@variable(m, i[i_Idx] <= 200, Int)
 setlowerbound(x[3], 0.001)
 setupperbound(x[3], 200.0)
 
