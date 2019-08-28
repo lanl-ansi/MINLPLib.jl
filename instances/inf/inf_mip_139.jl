880b8dbc68687e8d2b8ce616830e5d216230911e
Harsha Nagarajan <harsha.ngrjn@gmail.com>
2019-08-27 21:35:49 -0600

    [ci skip] clean_up

--- instances/inf/inf_mip_139.jl
+++ instances/inf/inf_mip_139.jl
@@ -6,11 +6,7 @@ m = Model()
 x_Idx = Any[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 @variable(m, x[x_Idx])
 b_Idx = Any[30, 31, 32, 33]
-@variable(m, b[b_Idx])
-setcategory(b[32], :Bin)
-setcategory(b[33], :Bin)
-setcategory(b[30], :Bin)
-setcategory(b[31], :Bin)
+@variable(m, b[b_Idx], Bin)
 
 
 # ----- Constraints ----- #
